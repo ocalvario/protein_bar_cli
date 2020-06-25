5d2674ab2cff185475a7f5213977267cad19ad1a
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-25 16:38:43 +0000

    add validation check

--- lib/protein_bar_cli/cli.rb
+++ lib/protein_bar_cli/cli.rb
@@ -1,21 +1,33 @@
 class ProteinBarCli::CLI 
   
   def call 
-    puts "Welcome to Protein Bar Raitings! Please select from the following protein bar options:"
-    get_bar_list
-    get_user_input
+    puts "\nWelcome to Protein Bar Raitings!\n"
+    get_bar_options
+    list_bar_options
+    get_user_selection
   end 
   
-  def get_bar_list
+  def get_bar_options
     # to be scraped instead
     @choices = ["Cliff Bar", "Builder Bar", "MetrRX", "think Bar"]
   end
   
-  def get_user_input
+  def list_bar_options
     #list bar choices
-    @choices.each_with_index  do |choice, index|
-      puts "#{index + 1}. #{choice}"
+    puts "\nPlease select one of the following protein bar options for more information:\n"
+    @choices.each.with_index(1)  do |choice, index|
+      puts "#{index}. #{choice}"
     end
   end
+  
+  def get_user_selection
+    bar_selection = gets.strip
+      if valid_input(bar_selection.to_i, @choices)
+      end
+  end
+  
+  def valid_input(input, data)
+    input <= @choices.length && input > 0
+  end  
   
 end 
