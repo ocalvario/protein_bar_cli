9fad3d5da1280892f9a602bf062cbb4f1efb7d5d
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-25 15:52:03 +0000

    started cli structure

--- lib/protein_bar_cli/cli.rb
+++ lib/protein_bar_cli/cli.rb
@@ -1,8 +1,21 @@
 class ProteinBarCli::CLI 
   
   def call 
-    puts "Welcome to Protein Bar Raitings!"
-    binding.pry
+    puts "Welcome to Protein Bar Raitings! Please select from the following protein bar options:"
+    get_bar_list
+    get_user_input
   end 
-
+  
+  def get_bar_list
+    # to be scraped instead
+    @choices = ["Cliff Bar", "Builder Bar", "MetrRX", "think Bar"]
+  end
+  
+  def get_user_input
+    #list bar choices
+    @choices.each_with_index  do |choice, index|
+      puts "#{index + 1}. #{choice}"
+    end
+  end
+  
 end 
